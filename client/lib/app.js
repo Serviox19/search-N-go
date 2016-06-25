200c65cc6f9a0ee3e2898134c1456caa5e92c7b5
Servio Mora <servio.mora19@gmail.com>
2016-06-24 23:41:38 -0400

    configured angular

---
+++ client/lib/app.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+var app = angular.module('startup', ['angular-meteor', 'ui.router']);
+
+app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function($urlRouterProvider, $stateProvider, $locationProvider){
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'client/views/home.ng.html',
+    controller: ''
+  })
+  .state('second',{
+    url: '/two',
+    templateUrl: 'client/views/second.ng.html',
+    controller: ''
+  })
+
+  $locationProvider.html5Mode(true);
+}]);
